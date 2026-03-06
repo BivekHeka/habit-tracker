e25bc71962047152f48dad3863b43238cb2d1816
Bivek Heka <bevillheka@gmail.com>
2026-03-07 00:04:57 +0545

    pixel added using post request & auto filling current date

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
+from datetime import datetime
+
 
 USERNAME = "heka10"
 TOKEN = "asdfgh123"
+GRAPH_ID = "graph1"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params ={
@@ -18,7 +21,7 @@ user_params ={
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id":"graph1",
+    "id":GRAPH_ID,
     "name":"Walk Step Graph",
     "unit":"km",
     "type":"float",
@@ -29,5 +32,18 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date":today.strftime("%Y%m%d"),
+    "quantity":"19000",
+}
+
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
